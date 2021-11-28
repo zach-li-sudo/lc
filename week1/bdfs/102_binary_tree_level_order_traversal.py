354ddf2d4acceda38be48370a1b21a360faadc80
zhuo <zhuoli@uvic.ca>
2021-11-28 16:17:24 +0800

    ds:tree, algo:bdfs

---
+++ week1/bdfs/102_binary_tree_level_order_traversal.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return
+        visited = set()
+        queue = []
+
+        if root not in visited:
+            queue.append(root.val)
+            queue += self.levelOrder(root.left)
+            queue += self.levelOrder(root.right)
+        
+        return queue
+        
+
